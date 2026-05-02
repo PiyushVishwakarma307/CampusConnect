5e75e9ad21aa35247f7aa1e543b7a50e3c443d8c
PiyushVishwakarma307 <piyushvishwakarma307@gmail.com>
2026-05-03 00:32:14 +0530

    final wala final pkka

--- src/main/java/com/piyush/campusconnect/security/CustomUserDetailsService.java
+++ src/main/java/com/piyush/campusconnect/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.piyush.campusconnect.security;
 
 import com.piyush.campusconnect.entity.User;
 import com.piyush.campusconnect.repository.UserRepo;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,14 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserRepo userRepository;
 
     @Override
+    @NullMarked
     public UserDetails loadUserByUsername(String rollNo) throws UsernameNotFoundException {
 
         User user = userRepository.findByRollNo(Integer.parseInt(rollNo)).orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(String.valueOf(user.getRollNo()))
-                .password(new String(user.getPassword()))
+                .password(user.getPassword())
                 .roles(user.getRole()) // IMPORTANT
                 .build();
     }
--- src/test/java/com/piyush/campusconnect/Test.java
+++ src/test/java/com/piyush/campusconnect/Test.java
@@ -5,6 +5,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Test {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("Piyush@123"));
+        String b = encoder.encode("Piyush@123");
+        System.out.println(b);
+        System.out.println((new String(b.getBytes())));
     }
 }
